89186e7acf417096afe3624903f87c3fd65824d6
Weliton Pereira Lima Junior <welitonlimadev@gmail.com>
2026-05-26 11:02:50 -0300

    feat: db main

--- app/main.py
+++ app/main.py
@@ -5,16 +5,22 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from mangum import Mangum
 
-
 from app.core.config import settings
+from app.db.base import Base
+from app.db.session import engine
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-app = FastAPI(
-    title="Mundo Invest API",
-    version="0.1.0",
-)
+async def lifespan(app: FastAPI):
+    if settings.env == "local":
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+    yield
+    await engine.dispose()
+
+
+app = FastAPI(title="Mundo Invest API", version="0.1.0", lifespan=lifespan)
 
 handler = Mangum(app, lifespan="off")
