22e7578bbf00625b71a37281e549ed761f6155ec
Weliton Pereira Lima Junior <welitonlimadev@gmail.com>
2026-05-26 10:59:44 -0300

    feat: db session

---
+++ app/db/session.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from app.core.config import settings
+
+engine = create_async_engine(
+    settings.database_url,
+    echo=settings.env == "local",
+    future=True,
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False,
+    autocommit=False,
+)
